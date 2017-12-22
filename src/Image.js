2197a0e1396a9a3c69adbcc16759b4a88749cf6f
github username <github email address>
2017-12-22 11:02:28 -0500

    complete

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,25 @@
 class Image {
-  
+  constructor(data){
+    this.id = data.id
+    this.url = data.url
+    this.name = data.name
+    this.like_count = data.like_count
+    this.comments = Comment.all // is an array of objects EDIT
+
+    Image.all.push(this)
+  }
+
+  static fetchImage(){
+    // console.log(Adapter.imageUrl())
+    Adapter.imageUrl().then(img => {
+      let image = document.querySelector("div#image_card.card.col-md-4")
+      // console.log(image);a
+      document.querySelector("img#image").src = `${img.url}`
+      document.querySelector("h4#name").innerText = `${img.name}`
+      document.querySelector("span#likes").innerText = `${parseInt(img.like_count)}`
+    })
+  }
 
 }
+
+Image.all = []
