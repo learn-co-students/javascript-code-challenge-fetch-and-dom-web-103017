490eeb08a3a0102c4148d0b56b6a55d9dc77e7d4
github username <github email address>
2017-12-22 11:01:04 -0500

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,22 @@
 class Comment {
-  
+
+  static addComment(){
+    let input = document.getElementById("comment_input").value
+    console.log(input)
+    let li = document.createElement('li')
+    li.innerHTML = `${input}`
+    document.getElementById("comments").append(li)
+    
+
+  }
+
+  static commentData(json){
+    let commentDiv = document.getElementById("comments")
+
+    json.comments.forEach(function(comment){
+      let li = document.createElement('li')
+      li.innerHTML = `${comment.content}`
+      commentDiv.append(li)
+    })
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 15 //Enter your assigned imageId here
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
   const likeURL = `https://randopic.herokuapp.com/likes/`
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+  fetch(`${imageURL}`)
+  .then(resp => resp.json())
+  .then(json => {
+    Image.imageData(json)
+    Comment.commentData(json)
+  })
+
+  document.getElementById("like_button").addEventListener('click', function(e){
+    e.preventDefault()
+    Image.like()
+    fetch(`${likeURL}`, {
+      method: "POST",
+      headers: { 'Accept': 'application/json',
+            'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({image_id: `${imageId}`})
+
+    })
+  })
+  document.getElementById("submit-comment").addEventListener('click',function(e){
+    e.preventDefault()
+    let content = document.getElementById("comment_input").value
+    Comment.addComment()
+    fetch(`${commentsURL}`, {
+      method: "POST",
+      headers: { 'Accept': 'application/json',
+            'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({image_id: `${imageId}`, content: `${content}`})
+    })
+  })
+
+
+
+
 
 })
