221f93d2465ead32d7e177557f2004b19fec4e30
dansbands <dansbands@hotmail.com>
2017-12-22 11:04:25 -0500

    grr

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,47 @@
+let allImages = []
+
 class Image {
-  
+  constructor(json) {
+    this.id = json.id
+    this.url = json.url
+    this.name = json.name
+    this.likeCount = json.like_count
+    this.comments = json.comments
+    allImages.push(this)
+  }
+
+///static methods
+
+  static all () {
+    return [...allImages]
+  }
+
+
+
+
+///this can be two separate methods, or I might not need to even create an image...
+
+///also, some of this should be instance methods, but do we really need any class methods, since there's only one image?
+  static createImage (json) {
+    let image = document.getElementById('image')
+    image.src = json.url
+
+    let name = document.getElementById('name')
+    name.innerHTML = json.name
+
+    let likes = document.getElementById('likes')
+    likes.innerHTML = json.like_count
+    //do I need this?
+    let newImage = new Image(json)
+  }
+
+  ////instance methods
+  addLikes() {
+
+  }
+
+  // render () {
+  //   return
+  // }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 6 //Enter your assigned imageId here
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-  const likeURL = `https://randopic.herokuapp.com/likes/`
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+  // const likeURL = `https://randopic.herokuapp.com/likes/`
+  // const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+
+  const commentsURL = `https://randopic.herokuapp.com/images/${imageId}`/
+
+
+
+///////Should I make an adapter class?
+
+
+///Get images
+  fetch(imageURL)
+  .then(resp => resp.json())
+  .then(json => Image.createImage(json))
+
+///Get comments
+  fetch(imageURL)
+  .then(resp => resp.json())
+  .then(json => Comment.createComment(json.comments))
+  // .then(json => Comment.createComment(json))
+
+///Update likes
+/// didn't finish this
+// Why wasn't there an adapter class?
+// don't show us a programming pattern an then change things at the last minute 
+  fetch(imageURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(json)
+  })
+
+  .then(resp => resp.json())
+  .then()
+
 
 })
